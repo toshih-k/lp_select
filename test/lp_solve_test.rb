cef8f525bf48a6af137bfe6675d3570f4136d8fc
Mattias Ekberg <mattias@mattiasekberg.com>
2015-07-27 07:56:49 +0200

    Added tests for new apis.

--- test/lp_solve_test.rb
+++ test/lp_solve_test.rb
@@ -185,8 +185,7 @@ class LpSolveTest < Test::Unit::TestCase
       end
     end
   end
-  
-  
+
 	# void delete_lp(lprec *lp);	
   def test_delete_lp
     @lp = LPSolve::make_lp(0, 1)
@@ -269,28 +268,53 @@ class LpSolveTest < Test::Unit::TestCase
     assert_equal 1.0, retvals[1]
   end
 
+  # unsigned char set_bounds(lprec *lp, int column, REAL lower, REAL upper);
   def test_set_bounds
     @lp = LPSolve::make_lp(0, 2)
     LPSolve::set_verbose(@lp, LPSolve::SEVERE )
-    result = LPSolve::set_bounds(@lp, 1, 0, 100)
-    assert_equal result, 1
 
-    result = LPSolve::set_bounds(@lp, 2, -10, 11)
-    assert_equal result, 1
+    assert_nothing_raised do
+      result = LPSolve::set_bounds(@lp, 1, 0, 100)
+      assert_equal result, 1
 
-    result = LPSolve::set_bounds(@lp, 3, -10, 11)
-    assert_equal result, 0
+      result = LPSolve::set_bounds(@lp, 2, -10, 11)
+      assert_equal result, 1
 
-    result = LPSolve::set_bounds(@lp, 1, -10, -11)
-    assert_equal result, 0
+      result = LPSolve::set_bounds(@lp, 3, -10, 11)
+      assert_equal result, 0
+
+      result = LPSolve::set_bounds(@lp, 1, -10, -11)
+      assert_equal result, 0
+    end
   end
 
+  # unsigned char set_add_rowmode(lprec *lp, unsigned char turnon);
   def test_set_add_rowmode
     @lp = LPSolve::make_lp(0, 1)
     LPSolve::set_verbose(@lp, LPSolve::SEVERE )
-    assert_equal LPSolve::set_add_rowmode(@lp, 1), 1
-    assert_equal LPSolve::set_add_rowmode(@lp, 1), 0
-    assert_equal LPSolve::set_add_rowmode(@lp, 0), 1
-    assert_equal LPSolve::set_add_rowmode(@lp, 0), 0
+    assert_nothing_raised do
+      assert_equal LPSolve::set_add_rowmode(@lp, 1), 1
+      assert_equal LPSolve::set_add_rowmode(@lp, 1), 0
+      assert_equal LPSolve::set_add_rowmode(@lp, 0), 1
+      assert_equal LPSolve::set_add_rowmode(@lp, 0), 0
+    end
+  end
+
+  # unsigned char set_obj(lprec *lp, int column, REAL value);
+  def test_set_obj
+    @lp = LPSolve::make_lp(0, 1)
+    LPSolve::set_verbose(@lp, LPSolve::SEVERE )
+    assert_nothing_raised do
+      LPSolve::set_obj(@lp, 1, 3.0)
+    end
+  end
+
+  # void set_timeout(lprec *lp, long sectimeout);
+  def test_set_timeout
+    @lp = LPSolve::make_lp(0, 1)
+    LPSolve::set_verbose(@lp, LPSolve::SEVERE )
+    assert_nothing_raised do
+      LPSolve::set_timeout(@lp, 3)
+    end
   end
 end
