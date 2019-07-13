27e570c11baaa0715d572d7874a75a11ba05b43b
Toshihisa KATO <toshihk@gmail.com>
2019-07-13 16:22:09 +0900

    add some functions

--- lib/lp_select/lp_solve.rb
+++ lib/lp_select/lp_solve.rb
@@ -1,17 +1,17 @@
 # -----------------------------------------------
-#  
+#
 #  This module uses Ruby's DL library to dynamically load
 #  the liblpsolve library, which is a linear equation solver.
 #
 #  The sourceforge summary says:
-#  Mixed Integer Linear Programming (MILP) solver lp_solve solves pure linear, 
-#  (mixed) integer/binary, semi-cont and special ordered sets (SOS) models.  
-#  lp_solve is written in ANSI C and can be compiled on many different platforms 
+#  Mixed Integer Linear Programming (MILP) solver lp_solve solves pure linear,
+#  (mixed) integer/binary, semi-cont and special ordered sets (SOS) models.
+#  lp_solve is written in ANSI C and can be compiled on many different platforms
 #  like Linux and WINDOWS
 #
 #  Read more at http://sourceforge.net/projects/lpsolve/
 #
-#  This module mostly serves to load the file and attach to the 
+#  This module mostly serves to load the file and attach to the
 #  API functions
 #
 # -----------------------------------------------
@@ -21,29 +21,29 @@ module LPSolve
   base = File.expand_path(File.join( File.dirname(__FILE__), "binaries") )
   err = nil
   ["liblpsolve55.so", "liblpsolve55.so-ux64", "liblpsolve55.dylib", "liblpsolve55.dylib.x86-64", "liblpsolve55.dylib-ppc", "lpsolve55.dll"].each do |lib|
-    begin 
+    begin
       err = nil
-      ffi_lib File.join(base, lib) 
+      ffi_lib File.join(base, lib)
       break
     rescue LoadError => e
       err = e
     end
   end
   raise "Could not find suitable liblpsolve55 library #{err}" unless err.nil?
-  
+
   #Constants used when assigning constraints
   LE = 1 # <=
   EQ = 3 # ==
   GE = 2 # >=
-  
+
   def op_to_s(op_int)
     case op_int
       when LPSelect::GE then ">="
-      when LPSelect::LE then "<=" 
+      when LPSelect::LE then "<="
       when LPSelect::EQ then "=="
     end
   end
-  
+
   #Constants used for verbosity
   CRITICAL = 1
   SEVERE = 2
@@ -51,7 +51,11 @@ module LPSolve
   NORMAL = 4
   DETAILED = 5
   FULL = 6
-  
+
+  #Constants for boolean
+  TRUE = 1
+  FALSE = 0
+
   #Constants used for solve results
   NOMEMORY = -2
   OPTIMAL = 0
@@ -72,81 +76,90 @@ module LPSolve
   typedef :long, :lprec
 
 
-  # All of the function signatures here come from 
+  # All of the function signatures here come from
   # http://lpsolve.sourceforge.net/5.5/lp_solveAPIreference.htm
 
   # void lp_solve_version(int *majorversion, int *minorversion, int *release, int *build)
   attach_function :lp_solve_version, [:pointer, :pointer, :pointer, :pointer], :void
-  
+
   # lprec *read_LP(char *filename, int verbose, char *lp_name)
   attach_function :read_LP, [:string, :int, :string], :pointer
-  
+
   # int solve(lprec *lp)
   attach_function :solve, [:pointer], :int
-  
+
   # unsigned char get_variables(lprec *lp, REAL *var);
   attach_function :get_variables, [:pointer, :pointer], :char
-  
+
   # int get_Ncolumns(lprec *lp);
   attach_function :get_Ncolumns, [:pointer], :int
-  
+
   # char *get_col_name(lprec *lp, int column);
   attach_function :get_col_name, [:pointer, :int], :string
-  
+
   # char *get_origcol_name(lprec *lp, int column);
   attach_function :get_origcol_name, [:pointer, :int], :string
-    
+
   # lprec *copy_lp(lprec *lp);
   attach_function :copy_lp, [:pointer], :pointer
-  
+
   # void print_lp(lprec *lp);
   attach_function :print_lp, [:pointer], :void
 
   # void void print_constraints(lprec *lp, int columns);
   attach_function :print_constraints, [:pointer, :int], :void
-  
+
   # lprec *make_lp(int rows, int columns);
   attach_function :make_lp, [:int, :int], :pointer
-  
+
   # void delete_lp(lprec *lp);
   attach_function :delete_lp, [:pointer], :void
-  
+
   # unsigned char set_binary(lprec *lp, int column, unsigned char must_be_bin);
   attach_function :set_binary, [:pointer, :int, :char], :char
-  
+
+  # unsigned char set_int(lprec *lp, int column, unsigned char must_be_int);
+  attach_function :set_int, [:pointer, :int, :char], :char
+
   # unsigned char set_col_name(lprec *lp, int column, char *new_name);
   attach_function :set_col_name, [:pointer, :int, :string], :char
-  
+
   # unsigned char set_obj_fn(lprec *lp, REAL *row);
   attach_function :set_obj_fn, [:pointer, :pointer], :char
 
   # unsigned char set_obj(lprec *lp, int column, REAL value);
   attach_function :set_obj, [:pointer, :int, :REAL], :char
-  
+
+  # unsigned char set_upbo(lprec *lp, int column, REAL value);
+  attach_function :set_upbo, [:pointer, :int, :REAL], :char
+
+  # unsigned char set_lowbo(lprec *lp, int column, REAL value);
+  attach_function :set_lowbo, [:pointer, :int, :REAL], :char
+
   # unsigned char add_constraint(lprec *lp, REAL *row, int constr_type, REAL rh);
   attach_function :add_constraint, [:pointer, :pointer, :int, :REAL], :char
 
   # unsigned char add_constraintex(lprec *lp, int count, REAL *row, int *colno, int constr_type, REAL rh);
   attach_function :add_constraintex, [:pointer, :int, :pointer, :pointer, :int, :REAL], :char
-  
+
   # unsigned char set_row_name(lprec *lp, int row, char *new_name);
   attach_function :set_row_name, [:pointer, :int, :string], :char
-  
+
   # REAL get_objective(lprec *lp);
   attach_function :get_objective, [:pointer], :REAL
-  
+
   # void set_verbose(lprec *lp, int verbose);
   attach_function :set_verbose, [:pointer, :int], :void
-  
+
   # unsigned char set_lp_name(lprec *lp, char *lpname);
   attach_function :set_lp_name, [:pointer, :string], :char
-  
+
   # unsigned char write_lp(lprec *lp, char *filename);
   attach_function :write_lp, [:pointer, :string], :char
-  
+
   # void set_maxim(lprec *lp);
   attach_function :set_maxim, [:pointer], :void
-  
+
   # void set_minim(lprec *lp);
   attach_function :set_minim, [:pointer], :void
 
@@ -166,8 +179,8 @@ module LPSolve
     rel_ptr = FFI::MemoryPointer.new(:pointer, 1)
     bld_ptr = FFI::MemoryPointer.new(:pointer, 1)
     LPSolve::lp_solve_version(maj_ptr, min_ptr, rel_ptr, bld_ptr)
-    
+
     "#{maj_ptr.read_int}.#{min_ptr.read_int}.#{rel_ptr.read_int} build #{bld_ptr.read_int}"
   end
-  
+
 end
--- test/lp_solve_test.rb
+++ test/lp_solve_test.rb
@@ -1,20 +1,20 @@
 require 'test_helper'
 
 class LpSolveTest < Test::Unit::TestCase
-  
+
   # void lp_solve_version(int *majorversion, int *minorversion, int *release, int *build)
   def test_lp_solve_version
     assert LPSolve.version.include?("5.5.0 build ")
   end
-  
+
   # lprec *make_lp(int rows, int columns);
   def test_make_lp
     assert_nothing_raised do
-      @lp = LPSolve::make_lp(0, 0) 
+      @lp = LPSolve::make_lp(0, 0)
     end
     assert_not_nil @lp
   end
-  
+
   # unsigned char set_binary(lprec *lp, int column, unsigned char must_be_bin);
   def test_set_binary
     @lp = LPSolve::make_lp(0, 1)
@@ -22,39 +22,63 @@ class LpSolveTest < Test::Unit::TestCase
       LPSolve::set_binary(@lp, 1, 1) #Define the column to be binary
     end
   end
-  
+
+  # unsigned char set_int(lprec *lp, int column, unsigned char must_be_int);
+  def test_set_int
+    @lp = LPSolve::make_lp(0, 1)
+    assert_nothing_raised do
+      LPSolve::set_int(@lp, 1, 1) #Define the column to be int
+    end
+  end
+
   # unsigned char set_col_name(lprec *lp, int column, char *new_name);
   def test_set_col_name
     @lp = LPSolve::make_lp(0, 1)
     assert_nothing_raised do
-      LPSolve::set_col_name(@lp, 1, "fred") 
+      LPSolve::set_col_name(@lp, 1, "fred")
     end
   end
-  
+
+  # unsigned char set_upbo(lprec *lp, int column, REAL value);
+  def test_set_upbo
+    @lp = LPSolve::make_lp(0, 1)
+    assert_nothing_raised do
+      LPSolve::set_upbo(@lp, 1, 1)
+    end
+  end
+
+  # unsigned char set_lowbo(lprec *lp, int column, REAL value);
+  def test_set_lowbo
+    @lp = LPSolve::make_lp(0, 1)
+    assert_nothing_raised do
+      LPSolve::set_lowbo(@lp, 1, 1)
+    end
+  end
+
   # unsigned char set_lp_name(lprec *lp, char *lpname);
   def test_set_lp_name
-    @lp = LPSolve::make_lp(0, 0) 
+    @lp = LPSolve::make_lp(0, 0)
     assert_nothing_raised do
       LPSolve::set_lp_name(@lp, "Hi mom")
     end
   end
-  
+
   # void set_verbose(lprec *lp, int verbose);
   def test_set_verbose
-    @lp = LPSolve::make_lp(0, 0) 
+    @lp = LPSolve::make_lp(0, 0)
     assert_nothing_raised do
       LPSolve::set_verbose(@lp, LPSolve::SEVERE )
     end
   end
-  
+
   # lprec *copy_lp(lprec *lp);
   def test_copy_lp
-    @lp = LPSolve::make_lp(0, 0) 
+    @lp = LPSolve::make_lp(0, 0)
     assert_nothing_raised do
       LPSolve::copy_lp(@lp)
     end
   end
-  
+
   # lprec *read_LP(char *filename, int verbose, char *lp_name)
   def test_read_lp
     filename = File.expand_path("../lp_format_sample.txt", __FILE__)
@@ -62,39 +86,39 @@ class LpSolveTest < Test::Unit::TestCase
       @lp = LPSolve::read_LP(filename, LPSolve::SEVERE, "")
     end
   end
-  
+
   # char *get_origcol_name(lprec *lp, int column);
   def test_get_origcol_name
     @lp = LPSolve::make_lp(0, 1)
     LPSolve::set_col_name(@lp, 1, "fred")
-    
+
     colname = LPSolve::get_origcol_name(@lp, 1)
     assert_equal "fred", colname.to_s
   end
 
   # void set_maxim(lprec *lp);
   def test_set_maxim
-    @lp = LPSolve::make_lp(0, 0) 
+    @lp = LPSolve::make_lp(0, 0)
     assert_nothing_raised do
       LPSolve::set_maxim(@lp)
     end
   end
-  
+
   # void set_minim(lprec *lp);
   def test_set_minim
-    @lp = LPSolve::make_lp(0, 0) 
+    @lp = LPSolve::make_lp(0, 0)
     assert_nothing_raised do
       LPSolve::set_minim(@lp)
     end
   end
-  
+
   # unsigned char write_lp(lprec *lp, char *filename);
   def test_write_lp
     destination = Tempfile.new("lp_solve_test")
     begin
       @lp = LPSolve::make_lp(0, 1)
       LPSolve::set_col_name(@lp, 1, "fred")
-    
+
       assert_nothing_raised do
         LPSolve::write_lp(@lp, destination.path)
       end
@@ -110,21 +134,21 @@ class LpSolveTest < Test::Unit::TestCase
     ncols = LPSolve::get_Ncolumns(@lp)
     assert_equal 3, ncols
   end
-  
+
 	# char *get_col_name(lprec *lp, int column);
   def test_get_col_name
     @lp = LPSolve::make_lp(0, 1)
     LPSolve::set_col_name(@lp, 1, "fred")
-    
+
     colname = LPSolve::get_origcol_name(@lp, 1)
     assert_equal "fred", colname.to_s
   end
-  
+
 	# unsigned char add_constraint(lprec *lp, REAL *row, int constr_type, REAL rh);
   def test_add_constraint
     @lp = LPSolve::make_lp(0, 2)
-    
-    #The API expects a 1 indexed array    
+
+    #The API expects a 1 indexed array
     constraint_vars = [0, 0, 1]
     FFI::MemoryPointer.new(:double, constraint_vars.size) do |p|
       p.write_array_of_double(constraint_vars)
@@ -153,29 +177,29 @@ class LpSolveTest < Test::Unit::TestCase
       end
     end
   end
-  
+
   # unsigned char set_row_name(lprec *lp, int row, char *new_name);
   def test_set_row_name
     @lp = LPSolve::make_lp(0, 2)
     LPSolve::set_col_name(@lp, 1, "fred")
     LPSolve::set_col_name(@lp, 2, "bob")
-    
+
     #The API expects a 1 indexed array
     constraint_vars = [0, 0, 1]
     FFI::MemoryPointer.new(:double, constraint_vars.size) do |p|
       p.write_array_of_double(constraint_vars)
       LPSolve::add_constraint(@lp, p, LPSelect::EQ, 1.0.to_f)
     end
-    
+
     assert_nothing_raised do
       LPSolve::set_row_name(@lp, 1, "onlyBob")
     end
   end
-  
+
   # unsigned char set_obj_fn(lprec *lp, REAL *row);
   def test_set_obj_fn
     @lp = LPSolve::make_lp(0, 1)
-    
+
     #The API expects a 1 indexed array
     constraint_vars = [1.0, 3.0]
     FFI::MemoryPointer.new(:double, constraint_vars.size) do |p|
@@ -186,21 +210,21 @@ class LpSolveTest < Test::Unit::TestCase
     end
   end
 
-	# void delete_lp(lprec *lp);	
+	# void delete_lp(lprec *lp);
   def test_delete_lp
     @lp = LPSolve::make_lp(0, 1)
     assert_nothing_raised do
       LPSolve::delete_lp(@lp)
     end
   end
-  
+
 	# int solve(lprec *lp)
   def test_solve
     @lp = LPSolve::make_lp(0, 2)
     LPSolve::set_verbose(@lp, LPSolve::SEVERE )
     LPSolve::set_col_name(@lp, 1, "fred")
     LPSolve::set_col_name(@lp, 2, "bob")
-    
+
     #The API expects a 1 indexed array
     constraint_vars = [0, 0, 1]
     FFI::MemoryPointer.new(:double, constraint_vars.size) do |p|
@@ -209,54 +233,54 @@ class LpSolveTest < Test::Unit::TestCase
     end
 
     LPSolve::set_minim(@lp)
-    
+
     assert_nothing_raised do
-      solution = LPSolve::solve(@lp) 
+      solution = LPSolve::solve(@lp)
     end
-    
+
   end
-  
+
   # REAL get_objective(lprec *lp);
   def test_get_objective
     @lp = LPSolve::make_lp(0, 2)
     LPSolve::set_verbose(@lp, LPSolve::SEVERE )
     LPSolve::set_col_name(@lp, 1, "fred")
     LPSolve::set_col_name(@lp, 2, "bob")
-    
+
     #The API expects a 1 indexed array
     constraint_vars = [0, 0, 1]
     FFI::MemoryPointer.new(:double, constraint_vars.size) do |p|
       p.write_array_of_double(constraint_vars)
       LPSolve::add_constraint(@lp, p, LPSelect::EQ, 1.0.to_f)
     end
-    
+
     LPSolve::set_minim(@lp)
-    solution = LPSolve::solve(@lp) 
-    
+    solution = LPSolve::solve(@lp)
+
     assert_nothing_raised do
       objective = LPSolve::get_objective(@lp)
       assert_not_nil objective
     end
   end
-  
-	# unsigned char get_variables(lprec *lp, REAL *var);  
+
+	# unsigned char get_variables(lprec *lp, REAL *var);
   def test_get_variables
     @lp = LPSolve::make_lp(0, 2)
     LPSolve::set_verbose(@lp, LPSolve::SEVERE )
     LPSolve::set_col_name(@lp, 1, "fred")
     LPSolve::set_col_name(@lp, 2, "bob")
-    
+
     #The API expects a 1 indexed array
     constraint_vars = [0, 0, 1]
     FFI::MemoryPointer.new(:double, constraint_vars.size) do |p|
       p.write_array_of_double(constraint_vars)
       LPSolve::add_constraint(@lp, p, LPSelect::EQ, 1.0.to_f)
     end
-    
-    
+
+
     LPSolve::set_minim(@lp)
-    solution = LPSolve::solve(@lp) 
-    
+    solution = LPSolve::solve(@lp)
+
     retvals = []
     FFI::MemoryPointer.new(:double, 2) do |p|
       assert_nothing_raised do
